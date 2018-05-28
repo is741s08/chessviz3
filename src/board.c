d6814694eba5def22f20de3bfaae4679f6157bee
Ubunt <ubunt@localhost.localdomain>
2018-05-29 04:33:53 +0700

    Add int coordinates

--- src/board.c
+++ src/board.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int coordinates(char i, int j, char i1, int j1) {
+    int g = 0;
+    char mass[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+    for (int k = 0; k <= 7; k++) {
+        if (i == mass[k]) {
+            g++;
+        }
+        if (i1 == mass[k]) {
+            g++;
+        }
+        if (j == k + 1) {
+            g++;
+        }
+        if (j1 == k + 1) {
+            g++;
+        }
+    }
+
+    if (g != 4) {
+        printf("Ошибка ввода данных\n");
+        return -1;
+    }
+    return 0;
+}

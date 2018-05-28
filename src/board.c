e0b7a872eb44129047fbfbc932b71ce5472725b2
Ubunt <ubunt@localhost.localdomain>
2018-05-29 05:01:09 +0700

    Add int convert

--- src/board.c
+++ src/board.c
@@ -25,3 +25,22 @@ int coordinates(char i, int j, char i1, int j1) {
     }
     return 0;
 }
+
+int convert(char i, int j, char i1, int j1) {
+    int index_i, index_i1;
+    static int index[4];
+    char mass[8] = {'A','B','C','D','E','F','G','H'};
+    for (int k = 0; k <= 7; k++) {
+    if (i == mass[k]) {
+        index_i = k;
+        }
+    if (i1 == mass[k]) {
+        index_i1 = k;
+        }
+    }
+    index[0] = index_i;
+    index[1] = j - 1;
+    index[2] = index_i1;
+    index[3] = j1 - 1;
+    return index;
+}
